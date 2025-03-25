ee5a6df48a7d73285ebf42d720aab95c859a4491
Mitra Kaipiainen <Mitro.Kaipiainen@kukkula.local>
2025-03-25 11:14:01 +0200

    starting javascript

---
+++ admin/java.js
@@ -0,0 +1,14 @@
+let editHeadingMode = false
+
+function handleHeadingEdit() {
+    console.log("Hi")
+
+    editHeadingMode = !editHeadingMode
+
+    if (editHeadingMode){
+        document.getElementById(companyTitle)
+    }
+}
+function handleParagraphEdit() {
+    console.log("Hello2")
+}
