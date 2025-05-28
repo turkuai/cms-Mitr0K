fd1f695776f36daa36370cd01a7df748ffcc8fef
Mitro Kaipiainen <mitro.kaipiainen@nidos.fi>
2025-05-28 15:36:15 +0300

    server connection safe and secure, task 10 done and dusted

--- tictactoe/db.php
+++ tictactoe/db.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tictactoe";
+foreach (parse_ini_file('.env') as $key => $value) {
+  $_ENV[$key] = $value;
+}
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+$conn = new mysqli(
+  $_ENV["DB_HOST"],
+  $_ENV["DB_USERNAME"],
+  $_ENV["DB_PASSWORD"],
+  $_ENV["DB_NAME"]
+);
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
